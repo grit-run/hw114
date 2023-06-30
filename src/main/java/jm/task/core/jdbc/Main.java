00e5a9dadcbc5b73d71d3260eb6e6f97d1b6c14a
Elinsky <dmitry.ilinsky@gmail.com>
2023-06-30 19:11:49 +0300

    private abd static improved.

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -4,9 +4,8 @@ import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
 
 public class Main {
-    private final static UserService userService = new UserServiceImpl();
-
     public static void main(String[] args) {
+        UserService userService = new UserServiceImpl();
         userService.createUsersTable();
 
         userService.saveUser("Tim", "Sad", (byte) 45);
